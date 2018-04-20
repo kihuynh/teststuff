33d8ea50ebeec785dfe83929dafba45aa69f28ab
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-20 13:40:11 -0700

    add create action with private params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,18 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(products_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+private
+  def products_params
+    params.require(:product).permit(:name, :cost, :origin)
+  end
 end
