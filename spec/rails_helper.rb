2b98b85adec577e1cdb744cede84dabbe9915be9
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-20 10:13:44 -0700

    add shoulda configurement to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,3 +55,10 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+Should::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rpsec
+    with.library :rails
+  end
+end
