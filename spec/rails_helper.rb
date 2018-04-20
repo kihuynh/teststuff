605d6bc2ff87dc4bc330bf22c89211776e384f4c
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-20 11:37:49 -0700

    fix typo of should to shoulda

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,7 +56,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 end
 
-Should::Matchers.configure do |config|
+Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rpsec
     with.library :rails
