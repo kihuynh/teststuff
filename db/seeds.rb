9e32b49e60af453500654fca4e87d504fe0be235
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-20 13:23:09 -0700

    add uniqueness to food dishes

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ Product.destroy_all
 
 50.times do |index|
   Product.create!(
-    name: Faker::Food.dish,
+    name: Faker::Food.unique.dish,
     cost: Faker::Commerce.price,
     origin: Faker::Address.country
     )
