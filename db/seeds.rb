79aaf0ac40b2a0ef3bf2da3e86895abbbd7fb21a
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-22 23:50:50 -0700

    seed reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 Product.destroy_all
+Review.destroy_all
 
 50.times do |index|
   Product.create!(
@@ -7,3 +8,15 @@ Product.destroy_all
     origin: Faker::Address.country
     )
 end
+
+5.times do |index|
+  Reviews.create!(
+    author: Faker::Book.unique.author,
+    content_body: Faker::Hipster.sentence(50),
+    rating: Faker::Number.between(1, 5),
+    product_id: @product_id
+    )
+end
+
+p "created #{Product.count} products"
+p "created #{Review.count} reviews"
