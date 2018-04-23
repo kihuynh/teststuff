a2743da8e0acfc521fb969669f79198ecec54844
Guest User <Guest@Epicodus-5C.local>
2018-04-23 11:44:12 -0700

    stuffing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,27 @@
 class Product < ActiveRecord::Base
   has_many :reviews
   validates :name, :presence => true
+
+  # scope :most_reviews, -> {(
+  #   select("products.id, products.name, products.image, products.countryoforigin, products.cost, count(reviews.id) as reviews_count")
+  #   .joins(:reviews)
+  #   .group("products.id")
+  #   .order("reviews_count DESC")
+  #   .limit(1)
+  # )}
+  #
+  # scope :id_sort, -> {(
+  #   select("products.id, products.name, products.image, products.countryoforigin, products.cost")
+  #   .group("products.id")
+  #   .order("products.id DESC")
+  #   .limit(3)
+  # )}
+  #
+  # scope :usa_origin, -> {(
+  #   select("products.id, products.name, products.image, products.countryoforigin, products.cost")
+  #   .group("products.id")
+  #   .where("products.countryoforigin = 'USA'")
+  #   .order("products.id DESC")
+  # )}
+
 end
